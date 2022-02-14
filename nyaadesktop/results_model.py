0a7cb8e31092c1295ca0df071abea440cacce7ea
mayudev <99561108+mayudev@users.noreply.github.com>
2022-02-14 14:43:11 +0100

    Small cosmetic touches to results list

--- nyaadesktop/results_model.py
+++ nyaadesktop/results_model.py
@@ -1,8 +1,8 @@
-from PySide6 import QtWidgets
 from PySide6.QtCore import Qt, QAbstractTableModel, QModelIndex, QSize, Slot
-from PySide6.QtGui import QColor, QIcon
+from PySide6.QtGui import QColor, QIcon, QFont
 
 from nyaadesktop.item import Item
+import re
 
 class ResultsModel(QAbstractTableModel):
     
@@ -32,13 +32,33 @@ class ResultsModel(QAbstractTableModel):
             return self.columns[section]
         else:
             return None
-            #return "{}".format(section)
 
     def data(self, index: QModelIndex, role = Qt.DisplayRole):
         column = index.column()
         row = index.row()
 
         if role == Qt.DisplayRole:
+            item = self.items[row]
+            match column:
+                case 0:
+                    return simplify_category(item.category)
+                case 1:
+                    return item.name
+                case 2:
+                    return item.size
+                case 3:
+                    return item.date
+                case 4:
+                    return item.seeders
+                case 5:
+                    return item.leechers
+                case 6:
+                    return item.completed
+                case 7:
+                    return item.comment_count
+                case _:
+                    return ""
+        elif role == Qt.ToolTipRole:
             item = self.items[row]
             match column:
                 case 0:
@@ -59,13 +79,32 @@ class ResultsModel(QAbstractTableModel):
                     return item.comment_count
                 case _:
                     return ""
+        elif role == Qt.StatusTipRole:
+            return self.items[row].name
+        elif role == Qt.BackgroundRole:
+            item = self.items[row]
+            if column == 0:
+                if item.category.startswith("Anime"):
+                    if item.category.endswith("Non-English-translated"):
+                        return QColor("#4e7c3a")
+                    elif item.category.endswith("English-translated"):
+                        return QColor("#683f7a")
+                    elif item.category.endswith("Raw"):
+                        return QColor("#66727a")
+                    else:
+                        return None
+        elif role == Qt.FontRole:
+            if column == 0:
+                font = QFont()
+                font.setBold(True)
+                return font
         elif role == Qt.SizeHintRole:
             return QSize(1, 28)
         elif role == Qt.DecorationRole:
             item = self.items[row]
             if column == 0:
                 if item.category.endswith("Non-English-translated"):
-                    return QIcon(":/icons/world")
+                    return QIcon(":/icons/es")
                 elif item.category.endswith("English-translated"):
                     return QIcon(":/icons/en")
                 elif item.category.endswith("Raw"):
@@ -73,11 +112,24 @@ class ResultsModel(QAbstractTableModel):
                 else:
                     return None
         elif role == Qt.ForegroundRole:
+            if column == 0:
+                item = self.items[row]
+
+                if item.category.startswith("Anime") and (item.category.endswith("translated") or item.category.endswith("Raw")):
+                    return QColor(Qt.white)
             if column == 4: # Seeders
                 return QColor(Qt.green)
             elif column == 5: # Leechers
                 return QColor(Qt.red)
-            
 
         return None
             
+def simplify_category(category: str) -> str:
+    if category.endswith("Non-English-translated"):
+        return re.sub(" - Non-English-translated", '', category)
+    elif category.endswith("English-translated"):
+        return re.sub( " - English-translated", '', category)
+    elif category.endswith("Raw"):
+        return re.sub( " - Raw", '', category)
+    else:
+        return category
