05cf4e2c1596312efc1238b6e655b45ccdeca708
mayudev <99561108+mayudev@users.noreply.github.com>
2022-02-18 13:06:03 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="NyaaDesktop",
-    version=0.1,
+    version=0.2,
     author="mayudev",
     description="A desktop application for nyaa.si",
     long_description=long_description,
